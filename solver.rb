5cdc32780a7f40d9ada3f530642f2fcbbdcce50a
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-11-17 19:51:53 +0500

    refactor code

--- solver.rb
+++ solver.rb
@@ -13,3 +13,20 @@ def fizzbuzz(number)
     number.to_s
   end
 end
+
+def reverse(word)
+  word.reverse
+end
+
+def fizzbuzz(number)
+  if (number % 3).zero? && (number % 5).zero?
+    'fizzbuzz'
+  elsif (number % 3).zero?
+    'fizz'
+  elsif (number % 5).zero?
+    'buzz'
+  else
+    number.to_s
+  end
+end
+end
