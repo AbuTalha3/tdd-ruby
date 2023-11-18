1e9f8d56c3ea5ca72ad7b4169019bc310165960f
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-11-18 19:29:20 +0500

    Removed syntax errors

--- solver.rb
+++ solver.rb
@@ -11,20 +11,20 @@ class Solver
       end
     end
 
-    
+
     result
   end
-  
+
   def reverse(word)
     word.reverse
   end
-  
+
   def fizzbuzz(number)
     return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
     return 'fizz' if (number % 3).zero?
     return 'buzz' if (number % 5).zero?
 
-    
+
     number.to_s
   end
 end
